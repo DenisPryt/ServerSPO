60a3acf90854c2ec88cb713c60e35c5741700658
DenisPryt <denis.chausy@gmail.com>
2015-12-13 16:56:53 +0300

    Initial Commit

---
+++ Server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Server
+//
+//  Created by Mac on 13.12.15.
+//  Copyright © 2015 spolk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
